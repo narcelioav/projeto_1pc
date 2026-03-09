34db8dfe67e9dd9ac2892b3752fe9c7b298f8594
narcelioav <narcelioav@gmail.com>
2026-03-09 18:53:29 -0300

    feat: implementa criação de pedidos via POST e teste com curl

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ server.listen(3000);
 console.log("Servidor rodando em http://localhost:3000");
 */
 
-const http = require("http");
+/*1const http = require("http");
 const cardapio = require("./data/cardapio");
 
 const server = http.createServer((req, res) => {
@@ -89,13 +89,12 @@ const server = http.createServer((req, res) => {
     } else if (req.url === "/cardapio") {
 
         res.writeHead(200, { 'Content-Type': 'application/json' });
-/*
-        const cardapio = [
+
+        /*const cardapio = [
             { "pizza": "calabresa", "preco": 35 },
             { "pizza": "frango", "preco": 38 }
-        ];
-*/
-        res.end(JSON.stringify(cardapio));
+        ];*/
+        /*2res.end(JSON.stringify(cardapio));
 
     }
     else if (req.url === "/sobre") {
@@ -118,6 +117,44 @@ const server = http.createServer((req, res) => {
 /*server.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });*/
+/*3
+server.listen(3000);
+
+console.log("Servidor rodando em http://localhost:3000");
+*/
+
+const http = require("http");
+const cardapio = require("./data/cardapio");
+const pedidos = [];
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/cardapio" && req.method === "GET") {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(cardapio));
+    }
+
+    else if (req.url == "/pedidos" && req.method === "GET") {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(pedidos));
+    }
+
+    else if (req.url === "/pedidos" && req.method === "POST") {
+
+        pedidos.push({
+            id: pedidos.length + 1,
+            pizza: "calabresa"
+        });
+
+        res.writeHead(201, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ mensagem: "Pedido criado!" }));
+    }
+
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("Rota não encontrada");
+    }
+});
 
 server.listen(3000);
 
